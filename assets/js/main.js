2b25aed880ac2c4bebcaadfe7afa48bc40c81285
Dian Sulistyo <50987253+deanzo13@users.noreply.github.com>
2022-08-24 11:00:18 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,3 +68,25 @@ const tabs = document.querySelectorAll('[data-target'),
         })
       })
       
+    // ---------------------- SERVICES MODAL ------------------------
+    const modalViews = document.querySelectorAll('.services__modal'),
+          modalBtns = document.querySelectorAll('.services__button'),
+          modalCloses = document.querySelectorAll('.services__modal-close')
+    
+    let modal = function(modalClick){
+        modalViews[modalClick].classList.add('active-modal')
+    }
+
+    modalBtns.forEach((modalBtns, i) => {
+        modalBtns.addEventListener('click', () =>{
+            modal(i)
+        })
+    })
+
+    modalCloses.forEach((modalCloses) => {
+        modalCloses.addEventListener('click', () =>{
+            modalViews.forEach((modalView) => {
+                modalView.classList.remove('active-modal')
+            })
+        })
+    })
